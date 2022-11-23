3077f20e7a5e273746a688e4a4c9cf4cadd7016c
Ryan Prendergast <rprendergast1121@gmail.com>
2022-11-22 23:18:42 -0600

    Smooth out line draw

--- canvas/canvas_loop.ts
+++ canvas/canvas_loop.ts
@@ -1,4 +1,9 @@
-let mouseX, mouseY, lastX, lastY: number
+interface Pixel {
+  x: number
+  y: number
+}
+
+let mouse: Pixel, last_mouse: Pixel
 let canvas: HTMLCanvasElement
 let ctx: CanvasRenderingContext2D
 
@@ -12,10 +17,8 @@ function subscribeToEvents(): void {
   if (subscribed_to_events) return
 
   canvas.addEventListener('mousemove', (e => {
-    lastX = mouseX
-    lastY = mouseY
-    mouseX = e.offsetX
-    mouseY = e.offsetY
+    if (mouse) last_mouse = {x: mouse.x, y: mouse.y}
+    mouse = {x: e.offsetX, y: e.offsetY}
   }))
 
   canvas.addEventListener('click', (e => {
@@ -35,12 +38,39 @@ export default function init() {
   requestAnimationFrame(update)
 }
 
+function drawPixel(pixel: Pixel, size = 2) {
+  ctx.fillRect(pixel.x, pixel.y, size, size)
+}
+
+/**
+ * Hitbox size is 2x2.
+ */
+function pixelsOverlapping(first, second): boolean {
+  const HITBOX_SIZE = 2
+
+  if (Math.abs(first.x - second.x) < HITBOX_SIZE && Math.abs(first.y - second.y) < HITBOX_SIZE) {
+    return true
+  }
+
+}
+
+function connectPixels(prev, curr) {
+  if (!prev || !curr) return
+
+  ctx.beginPath()
+  ctx.lineWidth = 4
+  ctx.moveTo(prev.x, prev.y)
+  ctx.lineTo(curr.x, curr.y)
+  ctx.stroke()
+}
+
 function update() {
   const hex = "#9342f5"
 
   if (drawing) {
     ctx.fillStyle = hex
-    ctx.fillRect(mouseX, mouseY, 2, 2)
+    ctx.strokeStyle = hex
+    connectPixels(last_mouse, mouse)
   }
 
   requestAnimationFrame(update)
--- components/homepage.tsx
+++ components/homepage.tsx
@@ -14,10 +14,12 @@ export function HomePage(props: HomePageProps): JSX.Element {
         <Terminal></Terminal>
       </Category>
       <Category name="music">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-evJxq7wCJPwhV7P91uooPOauMSX-UbE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <div>music. a youtube embed pollutes my site with blocked ad requests so you&apos;ll just have to imagine my <a target="blank" href="https://www.youtube.com/watch?v=zDsDCRVVCYw&list=PL-evJxq7wCJPwhV7P91uooPOauMSX-UbE">music</a></div>
+        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-evJxq7wCJPwhV7P91uooPOauMSX-UbE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
       </Category>
       <Category name="comedy">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-evJxq7wCJOAf5ktBD1geon3yeXGfA_8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <div>comedy. youtube embeds pollute my site with blocked ad requests so you&apos;ll just have to imagine my <a target="blank" href="https://www.youtube.com/watch?v=ePGkCFvNpM0&list=PL-evJxq7wCJOAf5ktBD1geon3yeXGfA_8">jokes</a></div>
+      {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL-evJxq7wCJOAf5ktBD1geon3yeXGfA_8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
       </Category>
     </div>
   )
